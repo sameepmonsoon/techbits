72496cfc1afa99b4e5acca0e904a523cb954f45d
Sameep Rana <sameep.monsoon@gmail.com>
2023-06-19 17:23:23 +0545

    usememo and usecallback implemented

--- src/Pages/ReducerPage.jsx
+++ src/Pages/ReducerPage.jsx
@@ -1,8 +1,50 @@
-import { useState } from "react";
+import { useId, useReducer, useState } from "react";
 import ReactQuill from "react-quill";
 import { toastMessageError } from "../Services/Toast Messages/ToastMessages";
-
+import Card from "../Components/Card/Card";
+import { AiFillDelete } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { v4 as uuid } from "uuid";
 const ReducerPage = () => {
+  const initialState = [
+    {
+      id: 1,
+      title: "",
+      photo: "",
+      body: "",
+    },
+  ];
+
+  function reducer(state, action) {
+    let updatedState;
+    const sameId =
+      state.length && state?.some((item) => item.id === action.payload.id);
+    switch (action.type) {
+      case "create":
+        if (sameId) {
+          console.log("i am same id", sameId);
+
+          updatedState = [...state];
+        } else {
+          console.log(
+            action.payload.id,
+            state.some((item) => item.id === action.payload.id)
+          );
+          updatedState = [...state];
+          updatedState?.push(action.payload);
+        }
+        return updatedState;
+      case "read":
+        return {};
+      case "update":
+        return {};
+      case "delete":
+        return {};
+      default:
+        throw new Error();
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [showForm, setShowForm] = useState(false);
   const [blogValue, setBlogValue] = useState({});
   const [formErrors, setFormErrors] = useState({});
@@ -12,13 +54,14 @@ const ReducerPage = () => {
   const handleChange = (event) => {
     const { value, name } = event.target;
     // console.log(value, name);
-
-    setBlogValue({ ...blogValue, [name]: value });
+    if (name === "title")
+      setBlogValue({ ...blogValue, [name]: value, id: uuid() });
+    else setBlogValue({ ...blogValue, [name]: value });
   };
   const handleImageChange = (event) => {
     var files = event?.target?.files[0];
     var reader = new FileReader();
-
+    const name = event.target.name;
     reader.onloadend = () => {
       setBlogValue({ ...blogValue, [name]: reader.result });
     };
@@ -30,8 +73,7 @@ const ReducerPage = () => {
     if (!values.title) {
       errors.title = "title can't be empty ";
     } else {
-      let titleRegEx = /^[a-zA-Z]$/;
-
+      let titleRegEx = /^[a-zA-Z]/;
       if (!titleRegEx.test(values.title)) {
         errors.title = "title must not contain numbers.";
       }
@@ -42,14 +84,18 @@ const ReducerPage = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(formValidation(blogValue));
-    setFormErrors(formValidation(e));
-
-    if (Object.keys(formErrors).length != 0) {
-      toastMessageError(formErrors.title);
+    if (Object.keys(formErrors).length !== 0) {
+      toastMessageError(formErrors?.title);
+    } else {
+      dispatch({ type: "create", payload: blogValue });
     }
   };
+
+  const handleBLogDelete = (blogId) => {
+    dispatch({ type: "delete", payload: blogId });
+  };
   return (
-    <div className="w-full h-[200vh] flex flex-col justify-start items-center p-1">
+    <div className="w-full h-[200vh] flex flex-col justify-start items-center p-1 gap-10">
       <div className="h-10 flex justify-center items-center w-auto">
         CRUD using useReducer
       </div>
@@ -62,7 +108,7 @@ const ReducerPage = () => {
       </div>
       {showForm && (
         <form
-          className="w-full p-2 h-auto  flex flex-col justify-start items-center gap-5"
+          className="w-[50%] p-2 h-auto  flex flex-col justify-start items-center gap-5 border-[1px] rounded-md"
           onSubmit={handleSubmit}>
           <textarea
             type="text"
@@ -72,17 +118,20 @@ const ReducerPage = () => {
             className="border-[1px] p-1 w-[50%]"
             onChange={handleChange}
           />
-          <img
-            src={blogValue?.photo && blogValue?.photo}
-            alt="No image chosen"
-            className="h-40 w-40 rounded-md border-[1px]"
-          />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            name="photo"
-          />
+          <div className="h-auto w-full flex flex-col justify-center items-start sm:flex-row">
+            <img
+              src={blogValue?.photo && blogValue?.photo}
+              alt="No image chosen"
+              className="h-40 w-40 rounded-md border-[1px] order-2 object-cover"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              name="photo"
+              className="w-[30%]"
+            />{" "}
+          </div>
           <div className="w-[50%] h-auto">
             <ReactQuill
               // value={editorContent}
@@ -106,14 +155,37 @@ const ReducerPage = () => {
           </div>
 
           <button className="w-40 h-10 border-[1px] rounded-md bg-gray-100 border-gray-300">
-            Submit
+            Publish
           </button>
         </form>
       )}
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, sed.
-        Voluptate cumque incidunt nulla aperiam. Mollitia deserunt libero quidem
-        enim eaque quo sequi iure culpa quis nostrum! Tenetur, ullam quod.
+      <div className="flex justify-center items-center gap-10 flex-wrap py-5">
+        {state.length &&
+          state
+            ?.filter((item) => item.title !== "")
+            .map((item, index) => (
+              <Card
+                key={index}
+                cardId={item.id}
+                cardTitle={item.title}
+                cardDescription={item.body}
+                cardUserName={
+                  <div className="flex gap-2 justify-start items-center w-full">
+                    <span className="cursor-pointer flex justify-center items-center gap-2 border-[1px] p-1 rounded-md w-20 hover:text-green-600 hover:border-green-600">
+                      Edit <FiEdit size={20} />
+                    </span>
+                    <span
+                      onClick={() => {
+                        handleBLogDelete(item?.id);
+                      }}
+                      className="cursor-pointer flex justify-start items-center gap-1 border-[1px] p-1 rounded-md hover:text-red-600 hover:border-red-600">
+                      Delete <AiFillDelete size={20} />
+                    </span>
+                  </div>
+                }
+                cardImage={item.photo}
+              />
+            ))}
       </div>
     </div>
   );
