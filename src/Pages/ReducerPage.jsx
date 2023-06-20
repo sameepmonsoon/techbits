ce81b51175381c90de17a44e98c8f798dfc75808
Sameep Rana <sameep.monsoon@gmail.com>
2023-06-20 10:39:29 +0545

    commit

--- src/Pages/ReducerPage.jsx
+++ src/Pages/ReducerPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useId, useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import ReactQuill from "react-quill";
 import { toastMessageError } from "../Services/Toast Messages/ToastMessages";
 import Card from "../Components/Card/Card";
@@ -6,8 +6,11 @@ import { AiFillDelete } from "react-icons/ai";
 import { FiEdit } from "react-icons/fi";
 import { v4 as uuid } from "uuid";
 const ReducerPage = () => {
-  const [localData, setLocalData] = useState(null);
-
+  const [showForm, setShowForm] = useState(false);
+  const [blogValue, setBlogValue] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+  const [enableEdit, setEnableEdit] = useState(false);
+  const [buttonType, setButtonType] = useState("Publish");
   const initialState = [
     {
       id: 1,
@@ -20,48 +23,70 @@ const ReducerPage = () => {
   function reducer(state, action) {
     let updatedState;
     const sameId =
-      state.length && state?.some((item) => item.id === action.payload.id);
+      state.length && state?.some((item) => item?.id === action.payload.id);
+    const blogExists = state?.find((item) => item?.id === action.payload);
+    const blogExistsForEdit = state?.find(
+      (item) => item?.id === action.payload.id
+    );
     switch (action.type) {
       case "create":
         if (sameId) {
-          console.log("i am same id", sameId);
-
           updatedState = [...state];
         } else {
-          console.log(
-            action.payload.id,
-            state.some((item) => item.id === action.payload.id)
-          );
           updatedState = [...state];
           updatedState?.push(action.payload);
         }
-        localStorage.setItem("localBlog", JSON.stringify(updatedState));
         return updatedState;
       case "read":
-        return {};
+        if (blogExists) {
+          setEnableEdit(true);
+          setButtonType("Update");
+          setBlogValue({
+            id: blogExists.id,
+            title: blogExists.title,
+            body: blogExists.body,
+            photo: blogExists.photo,
+          });
+          updatedState = [...state];
+        }
+        return updatedState;
       case "update":
-        return {};
+        if (blogExistsForEdit) {
+          blogExistsForEdit.id = action.payload.id;
+          blogExistsForEdit.title = action.payload.title;
+          blogExistsForEdit.body = action.payload.body;
+          blogExistsForEdit.photo = action.payload.photo;
+          updatedState = [...state, blogExists];
+          updatedState = state?.filter(
+            (item) => item?.id !== "" && item !== undefined
+          );
+          setEnableEdit(false);
+          setButtonType("publish");
+        }
+        return updatedState;
       case "delete":
-        console.log("to be deleted id", action.payload);
-        return {};
+        if (blogExists) {
+          updatedState = state?.filter((item) => item.id !== action.payload);
+        }
+        return updatedState;
       default:
         throw new Error();
     }
   }
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [showForm, setShowForm] = useState(false);
-  const [blogValue, setBlogValue] = useState({});
-  const [formErrors, setFormErrors] = useState({});
+
   const handleAddBlog = () => {
     setShowForm(true);
   };
-
   const handleChange = (event) => {
     const { value, name } = event.target;
-    // console.log(value, name);
-    if (name === "title")
-      setBlogValue({ ...blogValue, [name]: value, id: uuid() });
-    else setBlogValue({ ...blogValue, [name]: value });
+    if (enableEdit) {
+      setBlogValue({ ...blogValue, [name]: value });
+    } else {
+      if (name === "title")
+        setBlogValue({ ...blogValue, [name]: value, id: uuid() });
+      else setBlogValue({ ...blogValue, [name]: value });
+    }
   };
   const handleImageChange = (event) => {
     var files = event?.target?.files[0];
@@ -75,7 +100,7 @@ const ReducerPage = () => {
   };
   const formValidation = (values) => {
     let errors = {};
-    if (!values.title) {
+    if (!values?.title) {
       errors.title = "title can't be empty ";
     } else {
       let titleRegEx = /^[a-zA-Z]/;
@@ -92,20 +117,26 @@ const ReducerPage = () => {
     if (Object.keys(formErrors).length !== 0) {
       toastMessageError(formErrors?.title);
     } else {
-      dispatch({ type: "create", payload: blogValue });
+      if (enableEdit) {
+        dispatch({ type: "update", payload: blogValue });
+      }
+      if (state.length < 0 || !enableEdit)
+        dispatch({ type: "create", payload: blogValue });
     }
   };
 
   const handleBLogDelete = (blogId) => {
-    console.log("aaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa dddddd");
     dispatch({ type: "delete", payload: blogId });
   };
-
-  console.log("all state", state);
+  const handleBLogEdit = (blogId) => {
+    dispatch({ type: "read", payload: blogId });
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   useEffect(() => {
-    setLocalData(JSON.parse(localStorage.getItem("localBLog")));
-  }, [initialState]);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <div className="w-full h-[200vh] flex flex-col justify-start items-center p-1 gap-10">
       <div className="h-10 flex justify-center items-center w-auto">
@@ -123,6 +154,7 @@ const ReducerPage = () => {
           className="w-[50%] p-2 h-auto  flex flex-col justify-start items-center gap-5 border-[1px] rounded-md"
           onSubmit={handleSubmit}>
           <textarea
+            value={blogValue?.title}
             type="text"
             id="title"
             name="title"
@@ -146,7 +178,7 @@ const ReducerPage = () => {
           </div>
           <div className="w-[50%] h-auto">
             <ReactQuill
-              // value={editorContent}
+              value={blogValue?.body}
               onChange={(e) =>
                 handleChange({ target: { name: "body", value: e } })
               }
@@ -167,33 +199,39 @@ const ReducerPage = () => {
           </div>
 
           <button className="w-40 h-10 border-[1px] rounded-md bg-gray-100 border-gray-300">
-            Publish
+            {buttonType}
           </button>
         </form>
       )}
       <div className="flex justify-center items-center gap-10 flex-wrap py-5">
-        {state.length &&
+        {state?.length &&
           state
-            ?.filter((item) => item.title !== "")
+            ?.filter((item) => item?.title !== "")
             .map((item, index) => (
               <Card
                 key={index}
-                cardId={item.id}
-                cardTitle={item.title}
-                cardDescription={item.body}
+                cardId={item?.id}
+                cardTitle={item?.title}
+                cardDescription={item?.body}
                 cardUserName={
                   <div className="flex gap-2 justify-start items-center w-full">
-                    <span className="cursor-pointer flex justify-center items-center gap-2 border-[1px] p-1 rounded-md w-20 hover:text-green-600 hover:border-green-600">
+                    <span
+                      onClick={() => {
+                        handleBLogEdit(item?.id);
+                      }}
+                      className="cursor-pointer flex justify-center items-center gap-2 border-[1px] p-1 rounded-md w-20 hover:text-green-600 hover:border-green-600">
                       Edit <FiEdit size={20} />
                     </span>
                     <span
-                      onClick={handleBLogDelete}
+                      onClick={() => {
+                        handleBLogDelete(item?.id);
+                      }}
                       className="cursor-pointer flex justify-start items-center gap-1 border-[1px] p-1 rounded-md hover:text-red-600 hover:border-red-600">
                       Delete <AiFillDelete size={20} />
                     </span>
                   </div>
                 }
-                cardImage={item.photo}
+                cardImage={item?.photo}
               />
             ))}
       </div>
